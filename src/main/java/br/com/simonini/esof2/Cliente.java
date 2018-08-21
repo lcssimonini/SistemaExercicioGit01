b4108e4d8f05b0b9f530865935860965804731ec
xb213117 <p000lsimonini@prservicos.com.br>
2018-08-21 13:31:09 -0300

    movendo classe Cliente e adicionando campo slario

--- src/main/java/br/com/simonini/esof2/Cliente.java
+++
@@ -1,6 +0,0 @@
-package br.com.simonini.esof2;
-
-public class Cliente {
-
-    private String nome;
-}
---
+++ src/main/java/br/com/simonini/esof2/model/domain/Cliente.java
@@ -0,0 +1,8 @@
+package br.com.simonini.esof2.model.domain;
+
+public class Cliente {
+
+    private Integer id;
+    private String nome;
+    private Double salario;
+}
