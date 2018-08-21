7c1046a32d1ddc4479b6e8df31226f25798f5b64
xb213117 <p000lsimonini@prservicos.com.br>
2018-08-21 13:12:54 -0300

    versao inicial do sistema

---
+++ src/main/java/br/com/simonini/esof2/Esof2Application.java
@@ -0,0 +1,12 @@
+package br.com.simonini.esof2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Esof2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Esof2Application.class, args);
+	}
+}
